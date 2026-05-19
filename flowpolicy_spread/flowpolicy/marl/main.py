0b5f2d89974c72551d4c5a236575659b16461537
mnisyjs <1684730939@qq.com>
2026-05-19 13:52:44 +0800

    Update main.py

--- flowpolicy_spread/flowpolicy/marl/main.py
+++ flowpolicy_spread/flowpolicy/marl/main.py
@@ -1,12 +1,30 @@
 from common.arguments import get_args
 from envs.env_mpe import make_env
 from runner import Runner
+import numpy as np
+import random
+import torch
+import os
 load_path = None
 
-args = get_args()
-env, args = make_env(args)
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        # 针对 CuDNN 的确定性设置（保证在 GPU 上跑的结果也完全一致）
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
-runner = Runner(args, env)
-if load_path is not None:
-    runner.maddpg.load(load_path)
-runner.run()
+if __name__ == '__main__':
+    args = get_args()
+    set_seed(args.seed)
+    env, args = make_env(args)
+
+    runner = Runner(args, env)
+    if load_path is not None:
+        runner.maddpg.load(load_path)
+    runner.run()
